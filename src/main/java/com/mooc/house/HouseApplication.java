b3bbb8421107dd6fbb949e8961cd68d4143d8a4d
liqiuyan <380345904@qq.com>
2018-03-27 23:16:49 +0800

    css等静态文件 + 自定义starter

--- src/main/java/com/mooc/house/HouseApplication.java
+++ src/main/java/com/mooc/house/HouseApplication.java
@@ -1,9 +1,11 @@
 package com.mooc.house;
 
+import com1.mooc.house.autoconfig.EnableHttpClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableHttpClient
 public class HouseApplication {
 
 	public static void main(String[] args) {
